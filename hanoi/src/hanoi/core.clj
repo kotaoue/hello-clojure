6f25b0d5b7d46b256f1eec3d2a359afec536f74e
kotaoue <1369749+kotaoue@users.noreply.github.com>
2026-07-28 23:06:21 +0900

    Remove redundant comment in hanoi core

--- hanoi/src/hanoi/core.clj
+++ hanoi/src/hanoi/core.clj
@@ -6,7 +6,6 @@
     (hanoi (dec n) from via to)
     (println (str "Move disk " n " from " from " to " to))
      (hanoi (dec n) via to from))
-   ; 移動回数を返す
    (if (pos? n) (dec (Math/pow 2 n)) 0))
 
 (defn -main
