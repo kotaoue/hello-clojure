dad0e48488d43a6fb3b513765ae1cd72d74b8eb1
kotaoue <1369749+kotaoue@users.noreply.github.com>
2026-07-28 22:57:19 +0900

    Compute total moves from hanoi return value
    Refactored hanoi to return the move count (2^n - 1) instead of calculating it separately in -main. Updated -main to capture the return value directly.

--- hanoi/src/hanoi/core.clj
+++ hanoi/src/hanoi/core.clj
@@ -5,12 +5,13 @@
   (when (pos? n)
     (hanoi (dec n) from via to)
     (println (str "Move disk " n " from " from " to " to))
-    (hanoi (dec n) via to from)))
+     (hanoi (dec n) via to from))
+   ; 移動回数を返す
+   (if (pos? n) (dec (Math/pow 2 n)) 0))
 
 (defn -main
   [& args]
-  (let [n (if (seq args) (Integer/parseInt (first args)) 3)
-        total-moves (- (Math/pow 2 n) 1)]
-    (hanoi n "A" "C" "B")
+     (let [n (if (seq args) (Integer/parseInt (first args)) 3)
+       total-moves (hanoi n "A" "C" "B")]
     (println)
     (println (str "Total moves: " (int total-moves)))))
